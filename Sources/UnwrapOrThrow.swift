d6c09552f9c7984d0ed46b9e19b4415dcd40dd3f
Frizlab <fload@me.com>
2025-07-12 20:40:20 +0200

    Add support for typed throws

--- Sources/UnwrapOrThrow.swift
+++ Sources/UnwrapOrThrow.swift
@@ -11,4 +11,13 @@ public extension Optional {
 		return unwrapped
 	}
 	
+#if swift(>=6)
+	func unwrap<E : Error>(orThrow error: @autoclosure () -> E) throws(E) -> Wrapped {
+		guard let unwrapped = self else {
+			throw error()
+		}
+		return unwrapped
+	}
+#endif
+	
 }
--- Sources/UnwrapOrThrowOperator.swift
+++ Sources/UnwrapOrThrowOperator.swift
@@ -10,3 +10,12 @@ public func ?!<T>(lhs: T?, rhs: @autoclosure () -> Error) throws -> T {
 	}
 	return lhs
 }
+
+#if swift(>=6)
+public func ?!<T, E : Error>(lhs: T?, rhs: @autoclosure () -> E) throws(E) -> T {
+	guard let lhs = lhs else {
+		throw rhs()
+	}
+	return lhs
+}
+#endif
