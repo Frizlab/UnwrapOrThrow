e50842021057cb0d9c8e8456bcaf032505e6922a
Frizlab <fload@me.com>
2025-07-12 20:46:29 +0200

    Do not leave the non-typed throw variant when compiler is compatible with typed throws

--- Sources/UnwrapOrThrow.swift
+++ Sources/UnwrapOrThrow.swift
@@ -4,15 +4,15 @@ import Foundation
 
 public extension Optional {
 	
-	func unwrap(orThrow error: @autoclosure () -> Error) throws -> Wrapped {
+#if compiler(>=6)
+	func unwrap<E : Error>(orThrow error: @autoclosure () -> E) throws(E) -> Wrapped {
 		guard let unwrapped = self else {
 			throw error()
 		}
 		return unwrapped
 	}
-	
-#if compiler(>=6)
-	func unwrap<E : Error>(orThrow error: @autoclosure () -> E) throws(E) -> Wrapped {
+#else
+	func unwrap(orThrow error: @autoclosure () -> Error) throws -> Wrapped {
 		guard let unwrapped = self else {
 			throw error()
 		}
--- Sources/UnwrapOrThrowOperator.swift
+++ Sources/UnwrapOrThrowOperator.swift
@@ -4,15 +4,15 @@ import Foundation
 
 infix operator ?! : NilCoalescingPrecedence
 
-public func ?!<T>(lhs: T?, rhs: @autoclosure () -> Error) throws -> T {
+#if compiler(>=6)
+public func ?!<T, E : Error>(lhs: T?, rhs: @autoclosure () -> E) throws(E) -> T {
 	guard let lhs = lhs else {
 		throw rhs()
 	}
 	return lhs
 }
-
-#if compiler(>=6)
-public func ?!<T, E : Error>(lhs: T?, rhs: @autoclosure () -> E) throws(E) -> T {
+#else
+public func ?!<T>(lhs: T?, rhs: @autoclosure () -> Error) throws -> T {
 	guard let lhs = lhs else {
 		throw rhs()
 	}
